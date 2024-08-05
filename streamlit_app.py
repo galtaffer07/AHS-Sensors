6e55bbd666811df060cd89e09ca307789916ceac
galtaffer07 <114773751+galtaffer07@users.noreply.github.com>
2024-08-05 14:06:13 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,15 @@ import pandas as pd
 import numpy as np
 import datetime
 
-temperatures = pd.read_csv(r"C:\Users\galta\Downloads\AHS_2019_Temps.csv", skipfooter = 3, engine = 'python')
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+carbon_levels = pd.read_csv(r"C:\Users\galta\Downloads\2023 Q3Q4 AHS ZN-T & CO2 UTC.csv", skipfooter = 3, engine = 'python', index_col=0) 
+carbon_levels_without_empty_columns = carbon_levels[[c for c in carbon_levels.columns if not c.startswith('Unnamed: ')]]  #removes the 175 empty columns at end of dataset
+pd.DataFrame.iteritems = pd.DataFrame.items
+pd.set_option("display.max.columns", None)
+#carbon_levels.dropna(axis = 1, inplace = True)
+carbon_levels_without_empty_columns.dropna(axis = 0)
