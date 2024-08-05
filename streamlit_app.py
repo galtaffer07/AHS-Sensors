e60463f4e8b6fcbe6591120cc5173818992c82c1
galtaffer07 <114773751+galtaffer07@users.noreply.github.com>
2024-08-05 14:11:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import datetime
 
-st.title('CSV File Uploader')
+st.title('AHS CSV File')
 
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv") #forces user to upload file
+uploaded_file = st.file_uploader("Choose a CSV file that was uploaded from Metasys", type="csv") #forces user to upload file
 
 
 if uploaded_file is not None:  #checks to see if file uploaded
