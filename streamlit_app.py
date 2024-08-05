4bd9838e547b039f6bd8e367ba1168ebf179b026
galtaffer07 <114773751+galtaffer07@users.noreply.github.com>
2024-08-05 14:10:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,18 @@ import pandas as pd
 import numpy as np
 import datetime
 
+st.title('CSV File Uploader')
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
-carbon_levels = pd.read_csv(r"C:\Users\galta\Downloads\2023 Q3Q4 AHS ZN-T & CO2 UTC.csv", skipfooter = 3, engine = 'python', index_col=0) 
-carbon_levels_without_empty_columns = carbon_levels[[c for c in carbon_levels.columns if not c.startswith('Unnamed: ')]]  #removes the 175 empty columns at end of dataset
-pd.DataFrame.iteritems = pd.DataFrame.items
-pd.set_option("display.max.columns", None)
-#carbon_levels.dropna(axis = 1, inplace = True)
-carbon_levels_without_empty_columns.dropna(axis = 0)
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv") #forces user to upload file
+
+
+if uploaded_file is not None:  #checks to see if file uploaded
+    df = pd.read_csv(uploaded_file)
+
+    st.write("Here is your CSV file:")
+    st.dataframe(df)
+ 
+else:
+    st.write("Please upload a CSV file to proceed.") #if nothing then user is forced to upload
+
